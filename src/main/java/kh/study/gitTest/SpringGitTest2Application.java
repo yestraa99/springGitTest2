b882f36745849d49019cff3f27bae928ae8fe368
yestraa99 <https://github.com/yestraa99>
2022-10-26 16:04:34 +0900

    강의실테스트2

---
+++ src/main/java/kh/study/gitTest/SpringGitTest2Application.java
@@ -0,0 +1,13 @@
+package kh.study.gitTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitTest2Application.class, args);
+	}
+
+}
